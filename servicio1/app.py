0c3e10a1688f88e17bdfe4fec58a32f6bda70755
Rusevsk <99459871+Rusevsk@users.noreply.github.com>
2025-06-02 14:41:32 -0500

    actualizacion de app.py

--- servicio1/app.py
+++ servicio1/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola desde Servicio 1!"
+    return "¡Hola desde Servicio 1! --actualizado en el servicio 1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- servicio2/app.py
+++ servicio2/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola desde Servicio 2!"
+    return "¡Hola desde Servicio 2! --actualizado desde el servicio2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
